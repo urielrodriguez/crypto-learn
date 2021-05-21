21b364016e2564a7f20c016c46e530cc2e502db1
Uriel Rodriguez <uriel.rodriguez@icoe.org>
2021-05-21 16:12:42 -0700

    Implemented public and private key generator

---
+++ keys-api.py
@@ -0,0 +1,60 @@
+from bitcoinaddress import Wallet
+import flask
+from flask import request, jsonify
+from mnemonic import Mnemonic
+import binascii
+from flask_cors import CORS
+
+
+app = flask.Flask(__name__)
+CORS(app, support_credentials=True)
+
+@app.route('/keys-only', methods=['GET'])
+def get_keys():
+
+    wallet = Wallet()
+    public_key = wallet.address.__dict__['pubkeyc'][:24]
+    private_key = wallet.key.__dict__['hex'][:24]
+
+    keys = [
+        {'public_key': public_key,
+         'private_key': private_key
+        }
+    ]
+
+    response = jsonify(keys)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+@app.route('/keys-mnemonic', methods=['POST', 'GET'])
+def create_keys():
+
+    if request.method == 'POST':
+
+        data = request.get_json()
+        mnemonic_phrase = data['mnemonic_phrase']
+
+        public_key_mnemonic = mnemonic_phrase
+        private_key_mnemonic = mnemonic_phrase[::-1]
+
+        mn = Mnemonic('english')
+        public_key_seed = mn.to_seed(public_key_mnemonic)
+        private_key_seed = mn.to_seed(private_key_mnemonic)
+
+        public_key = str(binascii.hexlify(public_key_seed))[2:26]
+        private_key = str(binascii.hexlify(private_key_seed))[2:26]
+
+        print(public_key)
+        print(private_key)
+        
+        keys = [
+            {'public_key': public_key,
+            'private_key': private_key
+            }
+        ]
+
+        response = jsonify(keys)
+        return response    
+
+app.run()
--- py-request-test.js
+++ py-request-test.js
@@ -1,10 +1,11 @@
 function get_keys() {
     var req = new XMLHttpRequest();
-    req.open('GET', 'http://127.0.0.1:5000/keys', true);
+    req.open('GET', 'http://127.0.0.1:5000/keys-only', true);
     req.addEventListener('load', function() {
         if (req.status >= 200 && req.status < 400) {
             var response = JSON.parse(req.responseText);
-            console.log(response);
+            document.getElementById('public-key').textContent = 'Public Key: ' + response[0]['public_key']
+            document.getElementById('private-key').textContent = 'Private Key: ' + response[0]['private_key']
         }
         else {
             console.log('Error in network request: ' + req.statusText);
@@ -13,4 +14,35 @@ function get_keys() {
     req.send(null);
 }
 
-get_keys();
+document.getElementById('get-keys').addEventListener('click', function() {
+    get_keys();
+});
+
+
+function get_keys_mnemonic() {
+
+    var req = new XMLHttpRequest();
+    var payload = {mnemonic_phrase:null};
+    payload.mnemonic_phrase = document.getElementById('mnemonic-phrase').value;
+    req.open('POST', 'http://127.0.0.1:5000/keys-mnemonic', true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function() {
+        if (req.status >= 200 && req.status < 400) {
+            var response = JSON.parse(req.responseText);
+            console.log(response);
+            document.getElementById('public-key-mnemonic').textContent = 'Public Key: ' + response[0]['public_key']
+            document.getElementById('private-key-mnemonic').textContent = 'Private Key: ' + response[0]['private_key']
+        }
+        else {
+            console.log("Error in network request: " + req.statusText);
+        }
+    })
+    req.send(JSON.stringify(payload));
+    event.preventDefault();
+
+}
+
+
+document.getElementById('submit-mnemonic-phrase').addEventListener('click', function() {
+    get_keys_mnemonic();
+});
